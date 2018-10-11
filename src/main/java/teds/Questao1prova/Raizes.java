fc97311ed7c2094679e9cf1e058cf8fdd4009630
Andre <andreluiz97.bm@gmail.com>
2018-10-11 19:50:20 -0300

    primeiro commit

---
+++ src/main/java/teds/Questao1prova/Raizes.java
@@ -0,0 +1,17 @@
+package teds.Questao1prova;
+
+public class Raizes {
+	private double r1;
+	private double r2;
+	public Raizes(double r1, double r2) {
+		this.r1 = r1;
+		this.r2 = r2;
+	}
+	public double getR1() {
+		return r1;
+	}
+	public double getR2() {
+		return r2;
+	}
+
+}
